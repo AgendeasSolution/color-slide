73bd4d9279a4c5f5455ad8632897ef2087ba6cab
Shyam Mendapara <shyampmendapara@gmail.com>
2025-11-06 12:24:47 +0530

    Add Facebook SDK integration for enhanced social features

--- android/app/src/main/java/com/fgtp/color_slide/MainActivity.java
+++ android/app/src/main/java/com/fgtp/color_slide/MainActivity.java
@@ -1,6 +1,13 @@
 package com.fgtp.color_slide;
 
 import io.flutter.embedding.android.FlutterActivity;
+import com.facebook.FacebookSdk;
+import android.os.Bundle;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
 }
